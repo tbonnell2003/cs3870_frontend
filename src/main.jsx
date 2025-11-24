c294b65f4c86fde8ae534cfff7e7064416ee0d07
Tyler Bonnell <bonnellt@uwplatt.edu>
2025-11-24 15:56:18 -0600

    added basename in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* Add basename here so GitHub Pages works */}
+    <BrowserRouter basename="/cs3870_frontend">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
